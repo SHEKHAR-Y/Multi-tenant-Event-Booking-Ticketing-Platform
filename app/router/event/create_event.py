1b4b40e9dedff038e7f6fc3ba73a08180fa551c1
Shekhar <shekhar-android-dev@proton.me>
2026-09-02 22:31:17 +0530

    integrate: use get_current_user dependency instead of manual way

--- app/router/event/create_event.py
+++ app/router/event/create_event.py
@@ -1,21 +1,28 @@
 from fastapi import APIRouter, Depends, Request, status
 from fastapi.responses import JSONResponse
+
 from sqlalchemy.orm import Session
 
-from app.schemas.event import EventCreateRequest, EventCreateResponse
 from app.core.security import oauth2_scheme
-from app.services.event import create_event_service
 from app.core.database import get_db
+
+from app.services.event import create_event_service
+
+from app.dependencies.auth import get_current_user
+
+from app.schemas.event import EventCreateRequest, EventCreateResponse
+from app.models.user import User
+
 
 router = APIRouter()
 
 @router.post("/v1/event/create", response_model=EventCreateResponse, status_code=status.HTTP_201_CREATED)
-def create_event(request: Request, event_request: EventCreateRequest, token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+def create_event(request: Request, event_request: EventCreateRequest, current_user: User = Depends(get_current_user), db: Session = Depends(get_db)):
 
     # check if the user exist -> ensure token is valid if yess then just fetch user from token and fetch user role -> if role is organizer
     # then allow the user to create the event else raise error of forbidden 
 
-    res =  create_event_service(event_request, token, db)
+    res =  create_event_service(event_request, current_user, db)
     return EventCreateResponse(
         event_id=res.id,
         organizer_id=res.organizer_id,
--- app/services/event.py
+++ app/services/event.py
@@ -1,38 +1,30 @@
 from sqlalchemy.orm import Session
+
 import uuid 
 
-from app.schemas.event import EventCreateRequest
 from app.core.security import decode_access_token
-from app.repository.user import UserRepository
-from app.repository.event import EventRepository
 from app.core.exceptions import UserNotFound, UserNotAuthorized
-from app.models.user import UserRole
-from app.models.event import Event
 from app.core.db_error_handler import handle_db_error
 
-def create_event_service(event: EventCreateRequest, token: str, db: Session) -> Event:
-    user_repo = UserRepository(db)
+from app.schemas.event import EventCreateRequest
+
+from app.repository.user import UserRepository
+from app.repository.event import EventRepository
+
+from app.models.user import UserRole, User
+from app.models.event import Event
+
+
+def create_event_service(event: EventCreateRequest, current_user: User, db: Session) -> Event:
     event_repo = EventRepository(db)
 
-    # decode token and extract payload
-    payload = decode_access_token(token)
-
-    # from payload extract the user id entered as the subject in the token 
-    user_id = uuid.UUID(payload["sub"])  
-
-    # check if the user exist with the id 
-    result = user_repo.get_user_by_id(user_id)
-
-    if result is None:
-        raise UserNotFound("User not Found")
-
     # check if the user role is organizer
-    if result.role is not UserRole.ORGANIZER : 
+    if current_user.role is not UserRole.ORGANIZER : 
         raise UserNotAuthorized("User not authorized to perform this task")
 
     # event repo call to create the event
     new_event = Event(
-        organizer_id = result.id,
+        organizer_id = current_user.id,
         title = event.title,
         description = event.description,
         venue_name = event.venue_name,
