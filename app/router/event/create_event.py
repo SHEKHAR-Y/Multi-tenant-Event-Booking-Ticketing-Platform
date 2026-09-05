cb779054133050e78cf44c17065ffa06745d48cb
Shekhar <shekhar-android-dev@proton.me>
2026-09-05 17:16:20 +0530

    wrap event service in a class

--- app/router/event/create_event.py
+++ app/router/event/create_event.py
@@ -1,12 +1,10 @@
 from fastapi import APIRouter, Depends, Request, status
-from fastapi.responses import JSONResponse
 
 from sqlalchemy.orm import Session
 
-from app.core.security import oauth2_scheme
 from app.core.database import get_db
 
-from app.services.event import create_event_service
+from app.services.event import EventService
 
 from app.dependencies.auth import get_current_user
 
@@ -21,8 +19,9 @@ def create_event(request: Request, event_request: EventCreateRequest, current_us
 
     # check if the user exist -> ensure token is valid if yess then just fetch user from token and fetch user role -> if role is organizer
     # then allow the user to create the event else raise error of forbidden 
+    event_service = EventService(db)
+    res =  event_service.create_event_service(event_request, current_user)
 
-    res =  create_event_service(event_request, current_user, db)
     return EventCreateResponse(
         event_id=res.id,
         organizer_id=res.organizer_id,
--- app/services/event.py
+++ app/services/event.py
@@ -14,30 +14,31 @@ from app.repository.event import EventRepository
 from app.models.user import UserRole, User
 from app.models.event import Event
 
+class EventService:
+    def __init__(self, db: Session):
+        self.db = db
 
-def create_event_service(event: EventCreateRequest, current_user: User, db: Session) -> Event:
-    event_repo = EventRepository(db)
+    def create_event_service(self, event: EventCreateRequest, current_user: User) -> Event:
+        event_repo = EventRepository(self.db)
 
-    # check if the user role is organizer
-    if current_user.role is not UserRole.ORGANIZER : 
-        raise UserNotAuthorized("User not authorized to perform this task")
+        # check if the user role is organizer
+        if current_user.role is not UserRole.ORGANIZER : 
+            raise UserNotAuthorized("User not authorized to perform this task")
 
-    # event repo call to create the event
-    new_event = Event(
-        organizer_id = current_user.id,
-        title = event.title,
-        description = event.description,
-        venue_name = event.venue_name,
-        venue_address = event.venue_address,
-        start_time = event.start_time,
-        end_time = event.end_time 
-    )
+        # event repo call to create the event
+        new_event = Event(
+            organizer_id = current_user.id,
+            title = event.title,
+            description = event.description,
+            venue_name = event.venue_name,
+            venue_address = event.venue_address,
+            start_time = event.start_time,
+            end_time = event.end_time 
+        )
 
-    with handle_db_error(db): 
-        event_repo.create_event(new_event)
-        db.commit()
-        db.refresh(new_event)
+        with handle_db_error(self.db): 
+            event_repo.create_event(new_event)
+            self.db.commit()
+            self.db.refresh(new_event)
 
-    return new_event
-
-
+        return new_event
