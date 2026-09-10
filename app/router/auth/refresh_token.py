6e73b32ab3a073f0368ef6427d975ce95ffd1c54
Shekhar <shekhar-android-dev@proton.me>
2026-09-10 21:09:17 +0530

    add feature: rate limit

--- app/router/auth/refresh_token.py
+++ app/router/auth/refresh_token.py
@@ -3,12 +3,16 @@ from fastapi import APIRouter, Depends, Request, status
 from sqlalchemy.orm import Session
 
 from app.schemas.user import UserRefreshTokenRequest, UserRefreshTokenResponse
+
 from app.services.auth.user import UserService
+
 from app.core.database import get_db
+
+from app.dependencies.rate_limit import rate_limit
 
 
 router = APIRouter()
 
-@router.post("/v1/refresh_access_token", response_model=UserRefreshTokenResponse, status_code=status.HTTP_201_CREATED)
+@router.post("/v1/refresh_access_token", response_model=UserRefreshTokenResponse, status_code=status.HTTP_201_CREATED, dependencies=[Depends(rate_limit(requests=10, window_seconds=86400))])
 def refresh_access_token(request: Request, token_pair: UserRefreshTokenRequest, db: Session=Depends(get_db)):
     return UserService(db=db).refresh_access_token_service(refresh_token=token_pair.refresh_token)
